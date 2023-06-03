3b0d62a9b3f95f078f8d1082098d7a377b0823d4
zeeshan-aly <alisyedmohdzeeshan@gmail.com>
2023-06-03 16:32:57 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Zeeshan");
+        String name = rwOnly.getName();
+    }
 }
